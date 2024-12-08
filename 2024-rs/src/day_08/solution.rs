3d51af4c235dcc7c1609b9b179fd3d541170df91
Nurseiit Abdimomyn <nurseiit.abd@gmail.com>
2024-12-08 17:52:04 +0000

    solve day 8 part 2

--- 2024-rs/src/day_08/solution.rs
+++ 2024-rs/src/day_08/solution.rs
@@ -53,6 +53,59 @@ fn part_one() -> Result<()> {
     Ok(())
 }
 
+fn part_two() -> Result<()> {
+    let table = read_table_from_file("./src/day_08/input.txt")?;
+
+    let n = table.len() as i32;
+    let m = table.get(1).context("could not get the first row")?.len() as i32;
+
+    let mut chars: HashMap<char, Vec<(i32, i32)>> = HashMap::new();
+
+    table.iter().enumerate().for_each(|(i, row)| {
+        row.iter()
+            .enumerate()
+            .filter(|(_, char)| char.is_ascii_alphanumeric())
+            .for_each(|(j, char)| {
+                chars
+                    .entry(*char)
+                    .or_insert(vec![])
+                    .push((i as i32, j as i32))
+            })
+    });
+
+    let mut antinodes: HashSet<(i32, i32)> = HashSet::new();
+
+    chars.iter().for_each(|(_, positions)| {
+        positions.iter().for_each(|&first| {
+            // insert itself
+            antinodes.insert(first);
+
+            // loop through all produced antinodes with other chars
+            positions.iter().for_each(|&second| {
+                let (dx, dy) = (first.0 - second.0, first.1 - second.1);
+                if dx == 0 && dy == 0 {
+                    return;
+                }
+
+                let mut ni = first.0 + dx;
+                let mut nj = first.1 + dy;
+                loop {
+                    if ni < 0 || ni >= n || nj < 0 || nj >= m {
+                        break;
+                    }
+                    antinodes.insert((ni, nj));
+                    ni += dx;
+                    nj += dy;
+                }
+            })
+        });
+    });
+
+    println!("part two result {}", antinodes.len());
+
+    Ok(())
+}
+
 pub fn solve() -> Result<()> {
-    part_one()
+    part_two()
 }
