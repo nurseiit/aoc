3cfdea7d473d1508cad338ab676adb7ac9341864
Nurseiit Abdimomyn <nurseiit.abd@gmail.com>
2024-12-15 23:52:50 +0000

    solve day 15 part 1

--- 2024-rs/src/day_15/solution.rs
+++ 2024-rs/src/day_15/solution.rs
@@ -1,7 +1,7 @@
 use anyhow::{Context, Error, Ok, Result};
 use std::fs::read_to_string;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum TableItem {
     WALL,
     ROBOT,
@@ -71,10 +71,81 @@ fn read_document(file_name: &str) -> Result<Document> {
     Ok(Document { table, moves })
 }
 
-fn part_one() -> Result<()> {
-    let doc = read_document("simple")?;
+fn get_robot_position(table: &Vec<Vec<TableItem>>) -> (usize, usize) {
+    for i in 0..table.len() {
+        for j in 0..table[i].len() {
+            if table[i][j] == TableItem::ROBOT {
+                return (i, j);
+            }
+        }
+    }
+    (0, 0)
+}
 
-    println!("doc: {:?}", doc);
+fn move_in_direction(direction: &Direction, table: &mut Vec<Vec<TableItem>>) {
+    let (dx, dy) = match *direction {
+        Direction::UP => (-1, 0),
+        Direction::DOWN => (1, 0),
+        Direction::LEFT => (0, -1),
+        Direction::RIGHT => (0, 1),
+    };
+    let (i, j) = get_robot_position(&table);
+    let (nx, ny) = (i as i32 + dx, j as i32 + dy);
+
+    let n = table.len() as i32;
+    let m = table[0].len() as i32;
+
+    if nx < 0 || nx >= n || ny < 0 || ny >= m {
+        return;
+    }
+
+    let (ni, nj) = (nx as usize, ny as usize);
+
+    match table[ni][nj] {
+        TableItem::EMPTY => {
+            table[i][j] = TableItem::EMPTY;
+            table[ni][nj] = TableItem::ROBOT;
+        }
+        TableItem::WALL => (),
+        TableItem::BOX => {
+            let mut ci = ni;
+            let mut cj = nj;
+            while table[ci][cj] != TableItem::WALL && table[ci][cj] != TableItem::EMPTY {
+                ci = (ci as i32 + dx) as usize;
+                cj = (cj as i32 + dy) as usize;
+            }
+            if table[ci][cj] == TableItem::EMPTY {
+                table[i][j] = TableItem::EMPTY;
+                table[ni][nj] = TableItem::ROBOT;
+                table[ci][cj] = TableItem::BOX;
+            }
+        }
+        TableItem::ROBOT => panic!("found multiple robots!"),
+    }
+}
+
+fn part_one() -> Result<()> {
+    let doc = read_document("input")?;
+
+    let mut table = doc.table;
+    let moves = doc.moves;
+
+    for direction in moves {
+        move_in_direction(&direction, &mut table);
+    }
+
+    let mut result: i32 = 0;
+
+    for i in 0..table.len() {
+        for j in 0..table[i].len() {
+            if table[i][j] != TableItem::BOX {
+                continue;
+            }
+            result += i as i32 * 100 + j as i32;
+        }
+    }
+
+    println!("part one result {}", result);
 
     Ok(())
 }
