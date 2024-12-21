595ef14450b9b433c52e748eaa19a661f5c7f4dc
Nurseiit Abdimomyn <nurseiit.abd@gmail.com>
2024-12-21 02:20:02 +0000

    solve day2 both parts

--- 2024-rs/src/day_20/solution.rs
+++ 2024-rs/src/day_20/solution.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::{HashMap, HashSet, VecDeque},
     fs::read_to_string,
+    u32,
 };
 
 use anyhow::{anyhow, Context, Ok, Result};
@@ -45,16 +46,16 @@ fn get_neighbours((i, j): (usize, usize), (n, m): (usize, usize)) -> Vec<(usize,
     .collect()
 }
 
-fn shortest_path(
+fn shortest_path_distances(
     walls: &HashSet<(usize, usize)>,
     start: (usize, usize),
     finish: (usize, usize),
     dimensions: (usize, usize),
-) -> u32 {
+) -> HashMap<(usize, usize), u32> {
     let (n, m) = dimensions;
 
     let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
-    let mut dist: HashMap<(usize, usize), u32> = HashMap::new();
+    let mut dist = HashMap::new();
 
     queue.push_back(start);
     dist.insert(start, 0);
@@ -77,10 +78,18 @@ fn shortest_path(
             });
     }
 
-    *dist.get(&finish).expect("no path found!")
+    dist
+}
+
+fn manhattan_dist(from: (usize, usize), to: (usize, usize)) -> u32 {
+    let x = from.0.max(to.0) - from.0.min(to.0);
+    let y = from.1.max(to.1) - from.1.min(to.1);
+    (x + y) as u32
 }
 
 pub fn solve() -> Result<()> {
+    let max_cheat = 20;
+    let diff = 100;
     let maze = read_maze("input")?;
 
     let (n, m) = (maze.len(), maze[0].len());
@@ -89,46 +98,59 @@ pub fn solve() -> Result<()> {
     let mut finish = Default::default();
 
     let mut walls: HashSet<(usize, usize)> = HashSet::new();
+    let mut empties = vec![];
 
     for i in 0..n {
         for j in 0..m {
             match maze[i][j] {
                 MazeCell::Start => {
                     start = (i, j);
+                    empties.push((i, j));
                 }
                 MazeCell::Finish => {
                     finish = (i, j);
+                    empties.push((i, j));
                 }
                 MazeCell::Wall => {
                     walls.insert((i, j));
                 }
-                _ => {}
+                MazeCell::Empty => {
+                    empties.push((i, j));
+                }
             };
         }
     }
-    let original_shortest_path = shortest_path(&walls, start, finish, (n, m));
-    let diff = 100;
+    let dist_from_start = shortest_path_distances(&walls, start, finish, (n, m));
+    let mut result = 0;
 
-    let part_one_result = walls
-        .clone()
-        .into_iter()
-        .map(|(i, j)| {
-            vec![(i + 1, j), (i, j + 1)]
-                .into_iter()
-                .filter(|&(ni, nj)| ni < n && nj < m)
-                .filter(|neighbour| {
-                    if walls.remove(neighbour) {
-                        let current_path = shortest_path(&walls, start, finish, (n, m));
-                        walls.insert(*neighbour);
-                        return original_shortest_path - current_path >= diff;
-                    }
-                    false
-                })
-                .count()
-        })
-        .sum::<usize>();
+    for i in 0..empties.len() {
+        for j in 0..empties.len() {
+            if i == j {
+                continue;
+            }
+            let from = empties[i];
+            let to = empties[j];
+            let dist_to_from = *dist_from_start.get(&from).unwrap();
+            let dist_to_to = *dist_from_start.get(&to).unwrap();
+            let cheat = manhattan_dist(from, to);
 
-    println!("part one result: {}", part_one_result);
+            if cheat > max_cheat {
+                continue;
+            }
+
+            if dist_to_from + cheat >= dist_to_to {
+                continue;
+            }
+
+            if dist_to_from + cheat + diff > dist_to_to {
+                continue;
+            }
+
+            result += 1;
+        }
+    }
+
+    println!("result: {}", result);
 
     Ok(())
 }
